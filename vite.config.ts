96d631945f006d660bc731aee7440dad568fdbec
Sebastian Graz <graz@live.se>
2025-09-12 13:06:51 +0200

    kill postcss config

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,39 @@
-import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-import { defineConfig } from 'vite'
-import tsConfigPaths from 'vite-tsconfig-paths'
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import { defineConfig } from "vite";
+import tsConfigPaths from "vite-tsconfig-paths";
+import postcssCustomMedia from "postcss-custom-media";
+import postcssGlobalData from "@csstools/postcss-global-data";
+import postcssPresetEnv from "postcss-preset-env";
+import viteReact from "@vitejs/plugin-react";
 
 export default defineConfig({
+  css: {
+    postcss: {
+      plugins: [
+        postcssGlobalData({
+          files: ["./src/styles/global.css", "./src/styles/utils.css"],
+        }),
+        postcssCustomMedia(),
+        postcssPresetEnv({
+          autoprefixer: {
+            flexbox: "no-2009",
+          },
+          stage: 1,
+          features: {
+            "custom-media-queries": true,
+          },
+        }),
+      ],
+    },
+  },
   server: {
     port: 3000,
   },
   plugins: [
     tsConfigPaths({
-      projects: ['./tsconfig.json'],
+      projects: ["./tsconfig.json"],
     }),
     tanstackStart(),
+    viteReact(),
   ],
-})
+});
