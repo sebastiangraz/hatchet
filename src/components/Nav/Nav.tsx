01b73364a67297ac1fd0c3d775bff92a16ebbea1
Sebastian Graz <graz@live.se>
2025-09-20 20:35:31 +0200

    Add promobar

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,22 +1,25 @@
 import { Link } from "@tanstack/react-router";
 import { Logo } from "~/components/Logo/Logo";
-import { Text } from "~/components/Text/Text";
+import { Promobar } from "~/components/Promobar/Promobar";
 import styles from "./nav.module.css";
 
 export const Nav = () => {
   return (
-    <nav className={styles.nav}>
-      <div className={styles.wrapper}>
-        <Link to="/" className={styles.logo}>
-          <Logo />
-        </Link>
-        <menu className={styles.menu}>
-          <NavItem to="/">Home</NavItem>
-          <NavItem to="/customers">Customers</NavItem>
-          <NavItem to="/pricing">Pricing</NavItem>
-        </menu>
-      </div>
-    </nav>
+    <>
+      <Promobar />
+      <nav className={styles.nav}>
+        <div className={styles.wrapper}>
+          <Link to="/" className={styles.logo}>
+            <Logo />
+          </Link>
+          <menu className={styles.menu}>
+            <NavItem to="/">Home</NavItem>
+            <NavItem to="/customers">Customers</NavItem>
+            <NavItem to="/pricing">Pricing</NavItem>
+          </menu>
+        </div>
+      </nav>
+    </>
   );
 };
 
---
+++ src/components/Promobar/Promobar.tsx
@@ -0,0 +1,66 @@
+import styles from "./promobar.module.css";
+import { Text } from "~/components/Text/Text";
+import { Button } from "~/components/Button/Button";
+export const Promobar = () => {
+  return (
+    <div className={styles.promobar}>
+      <div className={styles.content}>
+        <SVGRocket />
+        <Text.Small>New feature launching soon - Stay tuned!</Text.Small>
+        {/* <Button icon="back" type="secondary">
+          Close
+        </Button> */}
+        <button
+          className={styles.closeButton}
+          type="button"
+          aria-label="Close promotion"
+        >
+          ×
+        </button>
+      </div>
+    </div>
+  );
+};
+
+const SVGRocket = () => {
+  return (
+    <svg
+      width="20"
+      height="21"
+      viewBox="0 0 20 21"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <line
+        x1="2.82843"
+        y1="12.975"
+        x2="4.94975"
+        y2="10.8537"
+        stroke="currentColor"
+        stroke-width="1.5"
+      />
+      <line
+        id="fire"
+        x1="2.82843"
+        y1="17.2177"
+        x2="7.07107"
+        y2="12.9751"
+        stroke="currentColor"
+        stroke-width="1.5"
+      />
+      <line
+        x1="7.07111"
+        y1="17.2177"
+        x2="9.19243"
+        y2="15.0964"
+        stroke="currentColor"
+        stroke-width="1.5"
+      />
+      <path
+        d="M15.9168 8.65356L10.6287 12.6435L7.40255 9.41729L11.3918 4.12849L15.5695 4.47652L15.9168 8.65356Z"
+        stroke="currentColor"
+        stroke-width="1.5"
+      />
+    </svg>
+  );
+};
